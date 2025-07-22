307a8ae2959091c3358e2c6d665d413e00400621
etsub-max <tsebaot1menber@gmail.com>
2025-07-22 13:51:24 +0300

    Add files via upload

---
+++ book2.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+class book{
+public:
+book() {
+cout<<"book is opened."<<endl;
+}
+~book(){
+cout<<"book is closed."<<endl;
+}	
+};
+int main() {
+book d;
+return 0;	
+}
